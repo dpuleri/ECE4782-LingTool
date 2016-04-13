8fc29e47399f49e11d2a7550eba0c7b509a6fd71
Daniel Puleri <daniel.puleri@gmail.com>
2016-04-12 22:11:25 -0400

    First notch filter usage

--- filter_audio.m
+++ filter_audio.m
@@ -1,5 +1,8 @@
 function st_filtered = filter_audio(st)
+    st_filtered = st;
+    H = notch60(st(1).Fs);
 % Get start and end of words
-
-
+    for i = 1:length(st)
+        st_filtered(i).audioFilt = filter(H,st(i).audio);
+    end
 end
