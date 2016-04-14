4b3eb40e7b36d71f86181d798d4f39fc2cc5ce05
Daniel Puleri <daniel.puleri@gmail.com>
2016-04-13 21:48:26 -0400

    Fix simple errors

--- match_ground_truth.m
+++ match_ground_truth.m
@@ -7,17 +7,17 @@ function ground_truth = match_ground_truth(st, raw_data)
 
 % output, ground truth with 1 if sleepy/tired and 0 if good
 
-    ground_truth = zeros(length(raw), 1); % pre-alloc
+    ground_truth = zeros(length(st), 1); % pre-alloc
     for i = 1:length(st)
         raw_data_ind = ceil(i/3); % map this to the raw data indices
  
         % if we have the previous day's data, take it into account
         if raw_data_ind > 1
-            sleep_hours = 0.75.*raw_data(raw_data_ind,2) + 0.25*.raw_data(raw_data_ind-1,2); % weighted
-            tired_feeling = 0.75.*raw_data(raw_data_ind,3) + 0.25*.raw_data(raw_data_ind-1,3); % weighted
+            sleep_hours = 0.75.*raw_data{raw_data_ind,2} + 0.25.*raw_data{raw_data_ind-1,2}; % weighted
+            tired_feeling = 0.75.*raw_data{raw_data_ind,3} + 0.25.*raw_data{raw_data_ind-1,3}; % weighted
         else
-            sleep_hours = raw_data(raw_data_ind,2); % un-weighted
-            tired_feeling = raw_data(raw_data_ind,3); % un-weighted
+            sleep_hours = raw_data{raw_data_ind,2}; % un-weighted
+            tired_feeling = raw_data{raw_data_ind,3}; % un-weighted
         end
         
         % assign value based on this threshold
